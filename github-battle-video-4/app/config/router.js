62d219793b7ed9d46b19b7ce8591f3fc0ada0b1b
Kara Satish Kumar <karasatishkumar@gmail.com>
2017-02-09 21:33:31 +0530

    chapter 3 and 4

---
+++ github-battle-video-4/app/config/router.js
@@ -0,0 +1,21 @@
+var React =  require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
+
+var routes = (
+	<Router history={hashHistory}>
+		<Route path='/' component={Main}>
+			<IndexRoute component={Home} />
+			<Route path='playerOne' header='player One' component={PromptContainer} />
+			<Route path='playerTwo/:playerOne' header='player Two' component={PromptContainer} />
+		</Route>
+	</Router>
+);
+
+module.exports = routes;
